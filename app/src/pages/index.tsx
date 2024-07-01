346a3f225c1757aacc00e6075f103c07d321309e
mbenz12 <mbenz10120112@gmail.com>
2024-07-01 12:49:32 -0400

    updated ux

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -12,19 +12,45 @@ export default function Home() {
   const program = useProgram();
   const [reload, setReload] = useState({});
   const { vault } = useFetchVault(reload);
-  const [amount, setAmount] = useState(0);
+  const [values, setValues] = useState<{ [key: string]: any }>({
+    mintAmount: 0,
+    combineAmounts: [0, 0, 0],
+    splitAmounts: [0, 0, 0],
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, index?: number) => {
+    let value = parseInt(e.target.value);
+    if (["combineAmounts", "splitAmounts"].includes(e.target.name)) {
+      const amounts = [...values[e.target.name]];
+      amounts[index!] = value;
+      setValues({
+        ...values,
+        [e.target.name]: amounts,
+      });
+    } else {
+      setValues({
+        ...values,
+        [e.target.name]: value,
+      });
+    }
+  }
 
   // console.log(vault?.fragmentSfts.map(sft => sft.mint.toString()));
 
   const handleMintSft = async () => {
     if (!program || !vault) return;
 
-    await mintSft(wallet, program, vault, new BN(amount))
+    if (!values.mintAmount) return;
+    
+    await mintSft(wallet, program, vault, new BN(values.mintAmount))
     setReload({});
   }
 
   const handleCombine = async (index: number) => {
     if (!program || !vault) return;
+
+    const amount = values.combineAmounts[index];
+    if (!amount) return;
 
     await combine(wallet, program, vault, index, new BN(amount));
     setReload({});
@@ -33,6 +59,9 @@ export default function Home() {
   const handleSplit = async (index: number) => {
     if (!program || !vault) return;
 
+    const amount = values.splitAmounts[index];
+    if (!amount) return;
+    
     await split(wallet, program, vault, index, new BN(amount));
     setReload({});
   }
@@ -43,16 +72,32 @@ export default function Home() {
         <WalletMultiButton />
       </div>
       {vault && <>
-          Amount: <input value={amount} onChange={(e) => setAmount(parseFloat(e.target.value) || 0.0)} type="number" />
-          <button onClick={handleMintSft}>Mint</button>
-          {vault.fragmentSfts.map((sft, index) => (
-            <div key={index} className='flex gap-2 items-center'>
-              {["Gold", "Silver", "Bronze"][index]}: {sft.mintedAmount.toString()} 
-              <button onClick={() => handleCombine(index)}>Combine</button>
-              <button onClick={() => handleSplit(index)}>Split</button>
-            </div>
-          ))}
-        </>
+        Amount: <input name="mintAmount" value={values.mintAmount} onChange={handleChange} type="number" min={0} />
+        <button onClick={handleMintSft}>Mint</button>
+        {vault.fragmentSfts.map((sft, index) => (
+          <div key={index} className='flex gap-2 items-center'>
+            {["Gold", "Silver", "Bronze"][index]}: {sft.mintedAmount.toString()} Fragment(s), {vault.pieceSfts[index].mintedAmount.toString()} Piece(s)
+            <input
+              name='combineAmounts'
+              value={values.combineAmounts[index]}
+              onChange={(e) => handleChange(e, index)}
+              type="number"
+              min={0}
+              max={vault.fragmentSfts[index].mintedAmount / 10}
+            />
+            <button onClick={() => handleCombine(index)}>Combine</button>
+            <input
+              name='splitAmounts'
+              value={values.splitAmounts[index]}
+              onChange={(e) => handleChange(e, index)}
+              type="number"
+              min={0}
+              max={vault.pieceSfts[index].mintedAmount}
+            />
+            <button onClick={() => handleSplit(index)}>Split</button>
+          </div>
+        ))}
+      </>
       }
     </div>
   )
